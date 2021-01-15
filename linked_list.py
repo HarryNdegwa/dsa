bfd7b0be012e2c3ab0cfbc8e229d33fa4206b5cb
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-15 19:45:09 +0300

    Added linked list insert value logic

--- linked_list.py
+++ linked_list.py
@@ -13,3 +13,17 @@ class LinkedList(object):
         else:
             self.head = None
 
+
+    def insert(self,value):
+        if self.head == None:
+            self.head = Node(value)
+        else:
+            self._insert(self.head,value)
+
+
+    def _insert(self,current_node,value):
+        if current_node.next_node == None:
+            current_node.next_node = Node(value)
+        else:
+            self._insert(current_node.next_node,value)
+
