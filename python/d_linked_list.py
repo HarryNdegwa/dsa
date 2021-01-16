f53607f29474d093b695356d0ea05a836c13eb1a
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-16 20:16:45 +0300

    Added insert center logic

--- python/d_linked_list.py
+++ python/d_linked_list.py
@@ -26,6 +26,37 @@ class DoublyLinkedList(object):
             self.head = Node(value)
 
 
+    def insert_center(self,value):
+        if self.head == None:
+            self.head = Node(value)
+        else:
+            center = round(self.size()/2)
+            self._insert_center(self.head,value,center)
+
+
+    def _insert_center(self,current_node,value,center,count=1):
+        if center == count:
+            next_ = current_node.next_node
+            current_node.next_node = Node(value)
+            current_node.next_node.next_node = next_
+        else:
+            self.insert_center(current_node.next_node,value,center,count+1)
+
+
+
+    def size(self):
+        if not self.head:
+            return 0
+        count_ = 0
+        current = self.head
+        while current:
+            count+=1
+            current = current.next_node
+        return current
+
+
+
+
     
 
 
